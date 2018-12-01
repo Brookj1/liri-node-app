88adfd5905beb2097ff12014befa4789c04c1130
Brookj1 <42814586+Brookj1@users.noreply.github.com>
2018-11-30 19:29:07 -0700

    fixing .gitignore

--- liri.js
+++ liri.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
 //added to format table 
-var spotify = new Spotify(keys.spotify);    
+var spotify = new Spotify(keys.spotify);
 var cTable = require("console.table");
 var request = require("request");
 var moment = require("moment");
@@ -24,13 +24,35 @@ if (process.argv[2] == 'concert-this') {
         console.log("Venue location: " + result.venue.city);
         console.log("Date of Event: " + moment(result.datetime).format("MM/DD/YYYY"));
     });
- 
+
 } else if (process.argv[2] == 'spotify-this-song') {
 
     var songName = process.argv.slice(3).join(" ");
 
-    if (songName == undefined) {
+    if (!songName) {
         songName = "The sign by Ace of Base";
+        spotify.search({ type: 'track', query: songName, limit: 10 }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+
+            var tableArray = [];
+
+            for (var i = 0; i < data.tracks.items.length; i++) {
+                var result = {
+                    artist: data.tracks.items[i].album.artists[0].name,
+                    album_name: data.tracks.items[i].album.name,
+                    song_name: data.tracks.items[i].name,
+                    preview_url: data.tracks.items[i].preview_url
+                }
+                tableArray.push(result);
+            }
+            var table = cTable.getTable(tableArray);
+
+            console.log(table);
+        });
+
+
     }
 
     spotify.search({ type: 'track', query: songName, limit: 10 }, function (err, data) {
@@ -56,28 +78,42 @@ if (process.argv[2] == 'concert-this') {
 
 } else if (process.argv[2] == 'movie-this') {
     var movieName = String(process.argv.slice(3));
-
-    // if (movieName == undefined) {
-    //     movieName = "Mr. Nobody";
-    // }
-    
     var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=de92683e";
-    axios.get(queryUrl).then(
 
-    function(response) {
-        
-        // var result = JSON.parse(body);
-        // console.log(response.data);
-        console.log("Title :" + response.data.Title);
-        console.log("Year :" + response.data.Released);
-        console.log("IMDB Rating :" + response.data.imdbRating);
-        console.log("Rotten Tomatoes :" + response.data.Ratings[1].Value);
-        console.log("Country :" + response.data.Country);
-        console.log("Language :" + response.data.Language);
-        console.log("Movie Plot :" + response.data.Plot);
-        console.log("Actors :" + response.data.Actors);
+    if (!movieName) {
+        movieName = "Mr. Nobody";
+        queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=de92683e";
+        axios.get(queryUrl).then(
 
-    });
+            function (response) {
+                console.log(queryUrl);
+                // console.log(response.data);
+                console.log("Title :" + response.data.Title);
+                console.log("Year :" + response.data.Released);
+                console.log("IMDB Rating :" + response.data.imdbRating);
+                console.log("Rotten Tomatoes :" + response.data.Ratings[1].Value);
+                console.log("Country :" + response.data.Country);
+                console.log("Language :" + response.data.Language);
+                console.log("Movie Plot :" + response.data.Plot);
+                console.log("Actors :" + response.data.Actors);
+
+            });
+    } else {
+        axios.get(queryUrl).then(
+            function (response) {
+                console.log(movieName);
+                // console.log(response.data);
+                console.log("Title :" + response.data.Title);
+                console.log("Year :" + response.data.Released);
+                console.log("IMDB Rating :" + response.data.imdbRating);
+                console.log("Rotten Tomatoes :" + response.data.Ratings[1].Value);
+                console.log("Country :" + response.data.Country);
+                console.log("Language :" + response.data.Language);
+                console.log("Movie Plot :" + response.data.Plot);
+                console.log("Actors :" + response.data.Actors);
+
+            });
+    }
 
 } else if (process.argv[2] == 'do-what-it-says') {
     console.log('do what it says')
